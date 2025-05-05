af3a2421a6c786f868be8787ae96732cdaf47c39
ioB147 <phucnbn.21it@vku.udn.vn>
2025-05-05 21:01:35 +0700

    --init

--- app/src/main/java/com/example/notemaster/MainActivity.kt
+++ app/src/main/java/com/example/notemaster/MainActivity.kt
@@ -12,7 +12,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.notemaster.ui.theme.NoteMasterTheme
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
